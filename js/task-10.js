875a992ce17debb4dab8286ac6cd07064e964187
VladimirPavlov1 <pavvvlovvvvvvladimir@gmail.com>
2022-10-20 21:02:03 +0300

    почав 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,58 @@
+// Напиши скрипт створення і очищення колекції елементів. Користувач вводить кількість елементів в input і натискає кнопку Створити, після чого рендериться колекція. Натисненням на кнопку Очистити, колекція елементів очищається.
+
+// <div id="controls">
+//   <input type="number" min="1" max="100" step="1" />
+//   <button type="button" data-create>Create</button>
+//   <button type="button" data-destroy>Destroy</button>
+// </div>
+
+// <div id="boxes"></div>
+
+// Створи функцію createBoxes(amount), яка приймає один параметр - число. Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
+
+// Розміри найпершого <div> - 30px на 30px.
+// Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
+// Всі елементи повинні мати випадковий колір фону у форматі HEX. Використовуй готову функцію getRandomHexColor для отримання кольору.
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+// Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const elementCreateControls= document.querySelector('#controls')
+console.dir(elementCreateControls)
+const refs = {
+  input:elementCreateControls.children[0],
+  btnCreate:elementCreateControls.children[1],
+  btnDestroy:elementCreateControls.children[2],
+  divBoxes:document.getElementById('boxes'),
+}
+console.dir(refs.divBoxes)
+function createBoxes(amount){
+   amount =Number(refs.input.value)
+  console.log(amount)
+  const createDiv=[]
+  for(let i=0;i<amount;i+=1){
+    console.log(i)
+    createDiv.push(document.createElement("div"))
+  }
+  refs.divBoxes.append(...createDiv)
+}
+const countDivesBoxes=Number(refs.divBoxes.childNodes.length)
+console.log(countDivesBoxes)
+
+function destroyBoxes(){
+for(let i=0; i=countDivesBoxes;i+=1){
+  console.log(countDivesBoxes)
+  remove()
+}
+}
+document.createElement("div")
+refs.btnCreate.addEventListener('click',createBoxes);
+refs.btnDestroy.addEventListener('click',destroyBoxes)
