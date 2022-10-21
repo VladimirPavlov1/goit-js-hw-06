c2bf8c408dec2b8ed32dde7d22b62f722f382af8
VladimirPavlov1 <pavvvlovvvvvvladimir@gmail.com>
2022-10-21 10:00:23 +0300

    виправив 2 завдання

--- js/task-02.js
+++ js/task-02.js
@@ -6,6 +6,14 @@
 // Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
 // Завдання 3
 
+// const listItems = ingredients.map(item => {
+//   const li = document.createElement('li')
+//   li.textContent = item;
+//   return li
+// })
+
+// listRef.append(...listItems)
+const itemsList=document.querySelector('#ingredients')
 
 const ingredients = [
   'Potatoes',
@@ -15,37 +23,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-const mainUlElem=document.querySelector('#ingredients')
 
+const listItems = ingredients.map(item=>{
+  const li =document.createElement('li')
+  li.textContent= item;
+ return li
+})
+itemsList.append(...listItems)
 
-const makeLiEl1= document.createElement('li');
-makeLiEl1.classList.add('item');
-makeLiEl1.textContent= ingredients[0];
-
-const makeLiEl2= document.createElement('li');
-makeLiEl2.classList.add('item');
-makeLiEl2.textContent= ingredients[1];
-
-const makeLiEl3= document.createElement('li');
-makeLiEl3.classList.add('item');
-makeLiEl3.textContent= ingredients[2];
-
-const makeLiEl4= document.createElement('li');
-makeLiEl4.classList.add('item');
-makeLiEl4.textContent= ingredients[3];
-
-
-const makeLiEl5= document.createElement('li');
-makeLiEl5.classList.add('item');
-makeLiEl5.textContent= ingredients[4];
-
-
-const makeLiEl6= document.createElement('li');
-makeLiEl6.classList.add('item');
-makeLiEl6.textContent= ingredients[5];
-
-
-mainUlElem.append(makeLiEl1,makeLiEl2,makeLiEl3,makeLiEl4,makeLiEl5,makeLiEl6)
-
-console.log(mainUlElem)
 
